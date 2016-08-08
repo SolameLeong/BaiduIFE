c1e0832c1f9e42380ce6152750af6c3be51d7c13
Solame <solameleong@outlook.com>
2016-08-08 22:25:30 +0800

    Finished.

--- task18/task18.js
+++ task18/task18.js
@@ -1,33 +1,53 @@
 /**
  * Created by solam on 2016/8/3.
  */
-//Left input handle
 var data = [];
 
 function renderImg() {
     var img = '';
     for (var i = 0; i < data.length; i ++) {
         var id = 'data' + i.toString();
-        img += '<div id=\"' + id + '\">' + data[i] + '</div>';
+        img += '<div id=\"' + id + '\" onclick=\"deleteFunc(this.id)\">' + data[i] + '</div>';
     }
     document.getElementById('chart').innerHTML = img;
 }
+function deleteFunc(id) {
+    for (var i = id - 1; i < data.length; i++) {
+        data[i - i] = data[i];
+    }
+    data.pop();
+}
+
 
 function leftIn() {
-    var input = document.getElementById('numIn').value;
-    console.log(input);
-    if (data.length !== 0) {
-        for (var i = data.length - 1; i >= 0; i--) {
-            data[i + 1] = data[i];
+    var input = document.getElementById('numIn');
+    var inputValue = Number(input.value);
+    if (Number.isInteger(inputValue) === true) {
+        if (data.length !== 0) {
+            for (var i = data.length - 1; i >= 0; i--) {
+                data[i + 1] = data[i];
+            }
+            data[0] = inputValue;
+        } else {
+            data[0] = inputValue;
         }
+        renderImg();
+    } else {
+        alert('Please enter integer!')
     }
-    data[0] = input;
-    renderImg();
+    input.value = null;
 }
 
 function rightIn() {
-    data[data.length] = document.getElementById('numIn').value;
-    renderImg();
+    var input = document.getElementById('numIn');
+    var inputValue = Number(input.value);
+    if (Number.isInteger(inputValue) === true) {
+        data[data.length] = inputValue;
+        renderImg();
+    } else {
+        alert('Please enter integer!');
+    }
+    input.value = null;
 }
 
 function leftOut() {
