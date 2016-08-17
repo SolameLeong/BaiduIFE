b95ddd8e779cdd32e57a188cd25e19d4b3d7d04c
Solame <solameleong@outlook.com>
2016-08-17 22:20:29 +0800

    Basic function finished, need to improve ui.

--- task17/task17.js
+++ task17/task17.js
@@ -45,170 +45,158 @@ var aqiSourceData = {
 // 用于渲染图表的数据
 var chartData = {};
 
-/**
- * 初始化图表需要的数据格式
- */
-
-function initAqiChartData() {
-    // 将原始的源数据处理成图表需要的数据格式
-    // 处理好的数据存到 chartData 中
-    function dayCalculator(){
-        chartData['day'] = aqiSourceData;
-    }
-
-    function weekCalculator() {
-        chartData['week'] = {};
-        for (var city in aqiSourceData) {
-            var i = 0, sum = 0, weekCount = 1;
-            var week = 'week';
-            chartData['week'][city] = {};
-            for (var date in aqiSourceData[city]){
-                if (i < 7) {
-                    sum += aqiSourceData[city][date];
-                    i ++;
-                } else {
-                    week = week.concat(weekCount.toString());
-                    var ave = sum / 7;
-                    chartData['week'][city][week] = ave;
-                    i = 0;
-                    sum = 0;
-                    weekCount ++;
-                }
-            }
-        }
-    }
-    function monthCalculator() {
-        chartData['month'] = {};
-        for (var city in aqiSourceData) {
-            var i = 0, sum = 0;
-            var month = '01';
-            chartData['month'][city] = {};
-            for (var date in aqiSourceData[city]) {
-                var today = city.slice(5, 8);
-                if (today === month) {
-                    sum += aqiSourceData[city][date];
-                    i ++;
-                } else {
-                    var ave = sum / i;
-                    chartData['month'][city][month] = ave;
-                    i = 0;
-                    sum = 0;
-                }
-            }
-        }
-    }
-    dayCalculator();
-    weekCalculator();
-    monthCalculator();
-}
-
 // 记录当前页面的表单选项
 var pageState = {
-    nowSelectCity: -1,
+    nowSelectCity: '北京',
     nowGraTime: "day"
 }
 
 /**
  * 渲染图表
  */
-function renderChart(time, city) {
-    var img = document.getElementById('aqi-chart-wrap').innerHTML;
-    var data = chartData[time][city];
-    var chart = document.getElementsByClassName('chart');
-    var count = 0;
-    for (var key in chartData[time][city]) {
-        img += '<div class=\"chart\" id = \"' + key + '\"></div>';
-        count ++;
+function renderChart(data) {
+    //html part
+    var area = document.getElementById('aqi-chart-wrap');
+    var chart = '', aqi;
+    for (var key in data) {
+        chart += '<div id=\"' + key + '\"></div>';
+    }
+    area.innerHTML = chart;
+    for (key in data) {
+        console.log(document.getElementById(key));
+        aqi = data[key].toString().concat('px');
+        document.getElementById(key).style.height = aqi;
     }
 
-    //style setting
-    var keys = Object.keys(chartData[time][city]);
-    console.log(keys);
-    for (var i = 0; i < count; i ++) {
-        if (data <= 50){
-            chart[i].style.backgroundColor = 'green';
-        } else if (data <= 100) {
-            chart[i].style.backgroundColor = 'yellow';
-        } else if (data <= 150) {
-            chart[i].style.backgroundColor = 'red';
-        } else if (data <= 200) {
-            chart[i].style.backgroundColor = 'purple';
-        } else {
-            chart[i].style.backgroundColor = 'black';
-        }
-        console.log(chartData[time][city][keys[i]]);
-        chart[i].style.height = chartData[time][city][keys[i]];
-    }
-
-    if (time === 'day') {
-        chart.style.width = '2px';
-    } else if (time === 'week') {
-        chart.style.width = '4px';
-    } else {
-        chart.style.width = '8px';
-    }
 }
 
 /**
  * 日、周、月的radio事件点击时的处理函数
  */
-var timeShow = 'day';
-function graTimeChange(input) {
+function graTimeChange() {
     // 确定是否选项发生了变化
-    if (input !== timeShow) {
-        timeShow = input;
+    console.log('time check');
+    var options = document.getElementsByName('gra-time');
+    for (var i = 0; i < options.length; i ++) {
+        if (options[i].checked) {
+            timeSpan = options[i].value;
+            break;
+        }
     }
-    // 设置对应数据
-    var opt = document.getElementById('city-select');
-    var city = opt.options[opt.selectedIndex].value;
-    // 调用图表渲染函数
-    renderChart(city, timeShow);
+    if (timeSpan !== pageState.nowGraTime) {
+        // 设置对应数据
+        pageState.nowGraTime = timeSpan;
+        var chart = chartData[timeSpan][pageState.nowSelectCity];
+        // 调用图表渲染函数
+        renderChart(chart);
+    }
 }
 
 /**
  * select发生变化时的处理函数
  */
-function citySelectChange() {
+function citySelectChange(s) {
     // 确定是否选项发生了变化
-    var opt = document.getElementById('city-select');
-    var city = opt.options[opt.selectedIndex].value;
-    // 设置对应数据
-    var timeOpt = document.getElementsByName('gra-time');
-    for (var i = 0; i < timeOpt.length; i++) {
-        if (timeOpt[i].checked === true) {
-            var time = timeOpt[i].value;
-            break;
-        }
+    var city = s[s.selectedIndex].id;
+    if (city !== pageState.nowSelectCity) {
+        pageState.nowSelectCity = city;
+        // 设置对应数据
+        var chart = chartData[pageState.nowGraTime][city];
+        // 调用图表渲染函数
+        renderChart(chart);
     }
-    // 调用图表渲染函数
-    renderChart(city, time);
 }
 
 /**
  * 初始化日、周、月的radio事件，当点击时，调用函数graTimeChange
  */
-//function initGraTimeForm() {
+function initGraTimeForm() {
+    var options = document.getElementsByName('gra-time');
+    for (var i = 0; i < options.length; i ++) {
+        options[i].onclick = graTimeChange;
+    }
+}
 
-//}
 /**
  * 初始化城市Select下拉选择框中的选项
  */
 function initCitySelector() {
     // 读取aqiSourceData中的城市，然后设置id为city-select的下拉列表中的选项
-    var option = '';
-    for (var key in aqiSourceData) {
-        option += '<option value=\"'+key+'\">' + key + '</option>';
+    var city = Object.keys(aqiSourceData);
+    var option = '<option></option>';
+    for (var i = 0; i < city.length; i ++) {
+        option += '<option id=\"' + city[i] + '\">' + city[i] + '</option>';
     }
     document.getElementById('city-select').innerHTML = option;
     // 给select设置事件，当选项发生变化时调用函数citySelectChange
 }
 
+/**
+ * 初始化图表需要的数据格式
+ */
+function initAqiChartData() {
+    // 将原始的源数据处理成图表需要的数据格式
+    // 处理好的数据存到 chartData 中
+    var i, count, sum, key, date;
+    var averageCount = {}, cityData = {};
+    //daily data
+    chartData['day'] = aqiSourceData;
+    //weekly data;
+    for (var city in aqiSourceData) {
+        i = 0;
+        count = 0;
+        sum = 0;
+        for (var days in aqiSourceData[city]) {
+            if (i < 7) {
+                sum += aqiSourceData[city][days];
+                i ++;
+            } else {
+                key = 'week'.concat(count.toString());
+                averageCount[key] = sum / 7;
+                count++;
+                sum = 0;
+                i = 0;
+            }
+        }
+        key = 'week'.concat(count.toString());
+        averageCount[key] = sum / 7;
+        cityData[city] = averageCount;
+    }
+    chartData['week'] = cityData;
+    //monthly data
+    averageCount = {};
+    cityData = {};
+    for (var cityM in aqiSourceData) {
+        i = 0;
+        count = 0;
+        sum = 0;
+        date = Object.keys(aqiSourceData[cityM]);
+        var month = date[0].substr(5, 2);
+        for (var dayM in aqiSourceData[cityM]) {
+            var dateMonth = date[i].substr(5, 2);
+            if (dateMonth === month) {
+                sum += aqiSourceData[cityM][dayM];
+                i ++;
+            } else {
+                key = 'month'.concat(count.toString());
+                averageCount[key] = sum / i;
+                count ++;
+                sum = 0;
+                i = 0;
+            }
+        }
+        key = 'month'.concat(count.toString());
+        averageCount[key] = sum / i;
+        cityData[city] = averageCount;
+    }
+    chartData['month'] = cityData;
+}
 
 /**
  * 初始化函数
  */
 function init() {
-    //initGraTimeForm();
+    initGraTimeForm();
     initCitySelector();
     initAqiChartData();
 }
